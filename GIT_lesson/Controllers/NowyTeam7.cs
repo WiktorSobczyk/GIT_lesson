b6aff2ab885150f6ddc980b1bfbf5fc06b2031f4
Adam Zielonka <adam.zielonka@outlook.com>
2015-11-03 16:03:06 +0100

    Zmina 214312412412

---
+++ GIT_lesson/Controllers/NowyTeam7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GIT_lesson.Controllers
+{
+    public class NowyTeam7 : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "COś";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Inne informacje";
+
+            return View();
+        }
+    }
+}
