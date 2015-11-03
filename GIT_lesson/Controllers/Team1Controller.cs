ad882abd0af7a21254b4f47379ed2c40564fc18a
tomalek116 <tomalek116@gmail.com>
2015-11-03 16:00:55 +0100

    Team1

---
+++ GIT_lesson/Controllers/Team1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using GIT_lesson.Models;
+
+namespace GIT_lesson.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public ActionResult ActionResult()
+        {
+            return View();
+        }
+    }
+}
